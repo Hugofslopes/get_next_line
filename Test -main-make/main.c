28bae569b7e52811da4cc8d42805d1a6a987ab73
hfilipe- <hfilipe-@student.42porto.com>
2024-11-19 16:56:50 +0000

    gnlfinal

--- Test -main-make/main.c
+++ Test -main-make/main.c
@@ -3,47 +3,203 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hfilipe- <hfilipe-@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: hfilipe- <hfilipe-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 20:08:13 by hfilipe-          #+#    #+#             */
-/*   Updated: 2024/11/13 17:21:40 by hfilipe-         ###   ########.fr       */
+/*   Updated: 2024/11/19 11:14:38 by hfilipe-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "get_next_line.h"
+#include "colors.h"
 
 int	main(void)
 {
 	int				fd;
 	char			*line;
 	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
-	//fd = open("test.txt", O_RDONLY);
-	fd = open("test.txt",O_RDONLY);
-	printf("%i\n",fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1 CHAR"RESET);
+	fd = open("1char", O_RDONLY);
 	line = get_next_line(fd);
-	printf("Test%u- %s", i++, line);
-		free (line);
-
-	line = get_next_line(2);
-	printf("Test%u- %s", i++, line);
-		free (line);
-
-	line = get_next_line(0);
-	printf("Test%u- %s", i++, line);
-		free (line);
-		
-	line = get_next_line(1);
-	printf("Test%u- %s", i++, line);
-		free (line);
-/* 	while (line != NULL)
-	{
-		printf("Test%u- %s", i++, line);
-		free (line);
-		line = get_next_line(fd);
-	} */
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
 	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1 CHAR - CALL TWICE"RESET);
+	fd = open("1char", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1 CHAR WITH NL"RESET);
+	fd = open("1char_with_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1 CHAR WITH NL- CALL TWICE"RESET);
+	fd = open("1char_with_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"EMPTY TWICE"RESET);
+	fd = open("empty", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"ONLY NL"RESET);
+	fd = open("newline", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"ONLY NL - TWICE"RESET);
+	fd = open("newline", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"TEXT NO NL"RESET);
+	fd = open("no_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	close(fd);
+	puts("\n");
+	puts(BG_BRIGHT_WHITE BOLD BLUE"TEXT NO NL - TWICE"RESET);
+	fd = open("no_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"TEXT WITH NL"RESET);
+	fd = open("with_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	close(fd);
+	puts("\n");
+	puts(BG_BRIGHT_WHITE BOLD BLUE"TEXT WITH NL - TWICE"RESET);
+	fd = open("with_nl", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"TEXT WITH 1CHAR 2ND LINE"RESET);
+	fd = open("with_nl_1char", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	close(fd);
+	puts("\n");
+	puts(BG_BRIGHT_WHITE BOLD BLUE"TEXT WITH 1CHAR 2ND LINE - 3TIMES"RESET);
+	fd = open("with_nl_1char", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1CHAR TXT 2ND LINE"RESET);
+	fd = open("1char_txt", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	close(fd);
+	puts("\n");
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1CHAR TXT 2ND LINE - 3TIMES"RESET);
+	fd = open("1char_txt", O_RDONLY);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	line = get_next_line(fd);
+	printf(GREEN "Test %u- "RESET, i++);
+	printf("%s", line);
+	free (line);
+	puts("\n");
+	close(fd);
+	puts(BG_BRIGHT_WHITE BOLD BLUE"1CHAR TXT 2ND LINE"RESET);
+	fd = open("big_txt", O_RDONLY);
+	j = 0;
+	while(j < 100)
+	{
+		line = get_next_line(fd);
+		printf(GREEN "Test %u- "RESET, i++);
+		printf("%s", line);
+		free (line);
+		j++;
+	}
 	return (0);
 }
--- letsgo/compile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-comp = cc -Wall -Wextra -Werror -D BUFFER_SIZE=4 get_next_line.c simple_main.c get_next_line_utils.c -o main -g
-$(COMP)
